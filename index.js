e9ac8cea3b3f3c5b3c8737aa6761557f91baa2ff
Daniel Rabinovich <dannyrabinovich@gmail.com>
2022-12-21 09:56:39 +0200

    small fixes

--- index.js
+++ index.js
@@ -23,26 +23,22 @@ let persons = [
     {
       "id": 1,
       "name": "Arto Hellas",
-      "number": "040-123456",
-      "show": true
+      "number": "040-123456"
     },
     {
       "id": 2,
       "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "show": true
+      "number": "39-44-5323523"
     },
     {
       "id": 3,
       "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "show": true
+      "number": "12-43-234345"
     },
     {
       "id": 4,
       "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "show": true
+      "number": "39-23-6423122"
     }
 ]
 
