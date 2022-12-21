cc76e2ff29b6817ae1cfe3193df0d34a6051de5c
Daniel Rabinovich <dannyrabinovich@gmail.com>
2022-12-21 10:44:19 +0200

    part 3.12

--- mongo.js
+++ mongo.js
@@ -1,34 +1,28 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length === 5) {
-  console.log('Please provide the password as an argument: node mongo.js <password> <name> <number>')
-  process.exit(1)
-}
-
-const password = process.argv[2]
-const name = process.argv[3]
-const number = process.argv[4]
-
-
+const len = process.argv.length
+const [,,password,name,number] = process.argv
 const url = `mongodb+srv://danny:${password}@phonebook.zp3rlwc.mongodb.net/?retryWrites=true&w=majority`
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String
 })
-
 const Person = mongoose.model('Person', personSchema)
 
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log('connected')
 
+if (len != 5 && len != 3) {
+  console.log('***Examples***')
+  console.log('post new person : node mongo.js <password> <name> <number>')
+  console.log('get all persons : node mongo.js <password>')
+  process.exit(1)
+} else if (len === 5) {
+  mongoose.connect(url)
+  .then((result) => {
     const person = new Person({
       name: name,
       number: number
     })
-
     return person.save()
   })
   .then(() => {
@@ -36,3 +30,29 @@ mongoose
     return mongoose.connection.close()
   })
   .catch((err) => console.log(err))
+
+} else if (len === 3) {
+  mongoose.connect(url)
+  .then(result => {
+     Person
+    .find({})
+    .then(result => {
+      console.log('phonebook')
+      result.forEach(p => console.log(`${p.name} ${p.number}`))
+      return mongoose.connection.close()
+    })
+  })
+  .catch((err) => console.log(err))
+
+} else {
+  console.log('something went wrong')
+}
+
+
+
+
+
+
+
+
+
